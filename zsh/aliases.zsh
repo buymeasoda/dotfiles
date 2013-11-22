dd901f947b303ab874ed8646002fdb2967bdd222
Ian Hill <ianhill@buymeasoda.com>
2013-11-21 23:26:12 -0800

    Add new aliases and functions

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -13,3 +13,11 @@ alias ythq="youtube-dl -t -f 22"
 alias tmd='(tmux has-session -t dev && tmux -CC attach-session -t dev) || tmux -CC new-session -s dev'
 alias tms='(tmux has-session -t scratch && tmux -CC attach-session -t scratch) || tmux -CC new-session -s scratch'
 alias tml='tmux ls'
+
+# General
+alias sd="ssh dev"
+alias doc="open ~/Documents"
+
+# Configuration
+alias dot="sub ~/.dotfiles ~/.oh-my-zsh ~/.vim ~/.ssh/config ~/.gitconfig ~/.gitignore ~/.zshrc ~/.vimrc ~/.dotfiles/zsh/*"
+alias doz="source ~/.zshrc"
--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -4,3 +4,11 @@ function server() {
     sleep 1 && open "http://localhost:${port}/"
     python -m SimpleHTTPServer "$port"
 }
+
+function show_colors () {
+    for code in {0..255}; do echo -e "\033[1;38;5;${code}m $code: Color code demonstration"; done
+}
+
+function cl() {
+	cd $1 && ls
+}
