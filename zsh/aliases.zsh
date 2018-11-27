a48afacd3199a0c3c29a930b17349b890cfb6d16
Ian Hill <ianhill@buymeasoda.com>
2018-11-26 16:48:20 -0800

    Update aliases and config

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,10 +5,9 @@ alias fn="find . -name"
 # Flush Directory Service cache
 alias flush="dscacheutil -flushcache"
 
-# Download youtube video (and youtube HQ)
+# Download youtube video
 # Usage: yt http://www.youtube.com/watch?v=abc123xyz098
-alias yt="youtube-dl -t -f 18"
-alias ythq="youtube-dl -t -f 22"
+alias yt="youtube-dl -f 'best[ext=mp4]'"
 
 # Tmux
 alias tmd='(tmux has-session -t dev && tmux -CC attach-session -t dev) 2> /dev/null || tmux -CC new-session -s dev'
@@ -19,6 +18,7 @@ alias tml='tmux ls'
 alias sd="ssh dev"
 alias doc="open ~/Documents"
 alias lap="open ~/Library/Application\ Support"
+alias notes=" atom ~/Desktop/Notes/"
 
 # Configuration
 alias dot="atom ~/.dotfiles ~/.oh-my-zsh"
