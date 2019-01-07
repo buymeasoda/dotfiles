c01310a6f51d5ba1ad9f717472560b5d59d89eb7
Ian Hill <ianhill@buymeasoda.com>
2019-01-06 16:39:55 -0800

    Add php server function

--- fish/functions/server.fish
+++ fish/functions/server.fish
@@ -1,4 +1,4 @@
-function server --argument port --description "Launch Python Simple HTTP Server and localhost browser for current directory"
+function server --argument port --description "Launch Python server and browser for current directory"
   if not test -n "$port"
     set port 8000
   end
