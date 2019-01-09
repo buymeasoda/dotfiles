1da3b381c7b1455eeb74e10488ea497ea47f97f6
Ian Hill <ianhill@buymeasoda.com>
2019-01-09 09:54:14 -0800

    Create conf.d and set config colors

---
+++ fish/conf.d/colors.fish
@@ -0,0 +1,26 @@
+set -g fish_color_autosuggestion 3a3a3a
+set -g fish_color_cancel normal
+set -g fish_color_command 9e9e9e
+set -g fish_color_comment 767676
+set -g fish_color_cwd 008000
+set -g fish_color_cwd_root 800000
+set -g fish_color_end 767676
+set -g fish_color_error d75f5f
+set -g fish_color_escape 00a6b2
+set -g fish_color_history_current normal
+set -g fish_color_host normal
+set -g fish_color_match normal
+set -g fish_color_normal normal
+set -g fish_color_operator 00a6b2
+set -g fish_color_param 00afaf
+set -g fish_color_quote 00afaf
+set -g fish_color_redirection 875faf
+set -g fish_color_search_match ffff00
+set -g fish_color_selection c0c0c0
+set -g fish_color_status red
+set -g fish_color_user 00ff00
+set -g fish_color_valid_path normal
+set -g fish_pager_color_completion normal
+set -g fish_pager_color_description yellow
+set -g fish_pager_color_prefix white --bold --underline
+set -g fish_pager_color_progress brwhite --background cyan
