2badd323c21a680af72b783d2b6abf108b4f39ff
Ian Hill <ianhill@buymeasoda.com>
2019-01-09 09:54:57 -0800

    Update main fish config with lscolors and defaults

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,12 @@
 # General
 set -x EDITOR /usr/bin/vim
 set -x VISUAL /usr/bin/vim
+set -x LSCOLORS Gxfxcxdxbxegedabagacad
 
 # Path
-set -x PATH /usr/local/opt/mysql@5.7/bin:$PATH
+set -x PATH /usr/local/opt/mysql@5.7/bin $PATH
 
 # Fish
+set -g fish_greeting
+set -g fish_key_bindings fish_default_key_bindings
 set -g fish_prompt_pwd_dir_length 0
