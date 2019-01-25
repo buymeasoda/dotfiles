c5b4e71a9becf17ede0f75ca8a542841130c566b
Ian Hill <ianhill@buymeasoda.com>
2019-01-24 16:50:48 -0800

    Improve server function to consolidate environments

--- fish/functions/phpserver.fish
+++
@@ -1,7 +0,0 @@
-function phpserver --argument port --description "Launch PHP server and browser for current directory"
-  if not test -n "$port"
-    set port 8000
-  end
-  open "http://localhost:$port/"
-  php -S "localhost:$port"
-end
--- fish/functions/server.fish
+++ fish/functions/server.fish
@@ -1,7 +1,25 @@
-function server --argument port --description "Launch Python server and browser for current directory"
-  if not test -n "$port"
-    set port 8000
-  end
+function server --description "Launch local server and browser for current directory"
+  argparse 'p/port=!_validate_int' 't/type=!_validate_server_type' -- $argv 2> /dev/null
+  or echo 'usage: server [-p|--port] <number> [-t|--type] <node|php|python>' && return
+
+  test -n "$_flag_port"; and set -l port $_flag_port; or set -l port 8000
+  test -n "$_flag_type"; and set -l type $_flag_type; or set -l type 'node'
+
   open "http://localhost:$port/"
-  python -m SimpleHTTPServer "$port"
+
+  switch $type
+    case node
+      http-server -p "$port"
+    case php
+      php -S "localhost:$port"
+    case python
+      python -m SimpleHTTPServer "$port"
+  end
+end
+
+function _validate_server_type  --no-scope-shadowing
+  if not string match -qr '^(node|php|python)$' -- $_flag_value
+    return 1
+  end
+  return 0
 end
