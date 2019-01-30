c5a0cf33e1999981df2fc17a2afa0532e640d50e
Ian Hill <ianhill@buymeasoda.com>
2019-01-29 23:24:04 -0800

    Add app installs to cask setup

--- setup/cask.sh
+++ setup/cask.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 
-# Setup
+echo
+echo "Installing application casks and fonts"
+echo
+
+# Tap
+brew tap caskroom/cask
 brew tap caskroom/fonts
 
 # Update
 brew cask upgrade
 
-# Install
+# Casks
+brew cask install android-file-transfer
+brew cask install firefox
+brew cask install google-backup-and-sync
+brew cask install google-chrome
+brew cask install imageoptim
+brew cask install istat-menus
+brew cask install iterm2
+brew cask install opera
+brew cask install private-internet-access
+brew cask install sketch
+brew cask install soulver
+brew cask install sourcetree
+brew cask install the-unarchiver
+brew cask install transmission
+brew cask install transmit
+brew cask install visual-studio-code
+brew cask install vlc
+brew cask install xscope
+
+# Fonts
 brew cask install font-meslo-for-powerline
