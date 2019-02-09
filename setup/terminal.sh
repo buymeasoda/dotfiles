0128c7a673e904de6706b3839b5b615dd3be72d5
Ian Hill <ianhill@buymeasoda.com>
2019-02-09 00:11:09 -0800

    Add default mac terminal theme config

--- setup/terminal.sh
+++ setup/terminal.sh
@@ -10,3 +10,9 @@ ln -sf ~/.dotfiles/home/.hushlogin ~/.hushlogin
 # iTerm
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iterm"
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
+# Terminal
+open ~/.dotfiles/iterm/Soda.terminal
+sleep 1
+defaults write com.apple.Terminal "Default Window Settings" -string "Soda"
+defaults write com.apple.Terminal "Startup Window Settings" -string "Soda"
