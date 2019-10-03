b6a90d55e10d0b8de1b1f4492964b077fc902b77
Ian Hill <ianhill@buymeasoda.com>
2019-10-03 22:35:08 +1000

    Adjust terminal and iTerm colour configuration

--- fish/conf.d/colors.fish
+++ fish/conf.d/colors.fish
@@ -31,7 +31,7 @@ set -l end (printf "\e[0m")
 set -x LESS_TERMCAP_mb (set_color red)
 set -x LESS_TERMCAP_md (set_color -o blue)
 set -x LESS_TERMCAP_me $end
-set -x LESS_TERMCAP_so (set_color brblack)
+set -x LESS_TERMCAP_so (set_color -u bryellow)
 set -x LESS_TERMCAP_se $end
 set -x LESS_TERMCAP_us (set_color -u magenta)
 set -x LESS_TERMCAP_ue $end
