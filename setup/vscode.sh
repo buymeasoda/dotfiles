702048c3e93311d3305a5bfaa756e38684911210
Ian Hill <ianhill@buymeasoda.com>
2021-04-01 19:18:21 +1000

    Update VS Code setup extension list

--- setup/vscode.sh
+++ setup/vscode.sh
@@ -5,12 +5,18 @@ echo "Configuring VS Code"
 echo
 
 # Extensions
+code --install-extension alefragnani.Bookmarks
 code --install-extension dbaeumer.vscode-eslint
+code --install-extension dzannotti.vscode-babel-coloring
 code --install-extension esbenp.prettier-vscode
 code --install-extension file-icons.file-icons
+code --install-extension flowtype.flow-for-vscode
 code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+code --install-extension Orta.vscode-jest
 code --install-extension skyapps.fish-vscode
+code --install-extension sourcegraph.sourcegraph
 code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension uloco.theme-bluloco-dark
 code --install-extension uloco.theme-bluloco-light
 code --install-extension vsmobile.vscode-react-native
 
