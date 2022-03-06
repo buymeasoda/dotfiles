a87e5437720c5cd65e93f45866eb6008e720ea2d
Ian Hill <ianhill@buymeasoda.com>
2022-03-06 18:00:47 +1000

    Update homebrew cask install script

--- setup/cask.sh
+++ setup/cask.sh
@@ -4,22 +4,16 @@ echo
 echo "Installing application casks and fonts"
 echo
 
-# Tap
-brew tap caskroom/cask
-brew tap caskroom/fonts
-brew tap AdoptOpenJDK/openjdk
-
 # Update
-brew cask upgrade
+brew upgrade --cask
 
 # Casks
 brew install --cask adobe-acrobat-reader
-brew install --cask adoptopenjdk8
+brew install --cask adoptopenjdk/openjdk/adoptopenjdk11
 brew install --cask android-file-transfer
 brew install --cask android-studio
 brew install --cask calibre
 brew install --cask firefox
-brew install --cask google-backup-and-sync
 brew install --cask google-chrome
 brew install --cask imageoptim
 brew install --cask istat-menus
@@ -31,7 +25,6 @@ brew install --cask private-internet-access
 brew install --cask qlmarkdown
 brew install --cask sketch
 brew install --cask soulver
-brew install --cask sourcetree
 brew install --cask the-unarchiver
 brew install --cask transmission
 brew install --cask transmit
@@ -40,4 +33,4 @@ brew install --cask vlc
 brew install --cask xscope
 
 # Fonts
-brew install --cask font-meslo-for-powerline
+brew install --cask homebrew/cask-fonts/font-meslo-for-powerline
