93c58b6ff459b3f560a46eabf8a4d44215b3bf29
Ian Hill <ianhill@buymeasoda.com>
2022-03-18 17:15:30 +1000

    Add android sdk config and path

---
+++ fish/conf.d/android.fish
@@ -0,0 +1,9 @@
+# Android Tools (Install via Android Studio)
+set -x ANDROID_HOME $HOME/Library/Android/sdk
+
+# Paths
+path $ANDROID_HOME/emulator
+path $ANDROID_HOME/tools
+path $ANDROID_HOME/cmdline-tools/latest/bin
+path $ANDROID_HOME/tools/bin
+path $ANDROID_HOME/platform-tools
