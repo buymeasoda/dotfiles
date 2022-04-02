395fe758a1f603f06538fa895ea4abce4a4f58c3
Ian Hill <ianhill@buymeasoda.com>
2022-04-02 15:15:57 +1000

    Update VS Code setup extension list

--- setup/vscode.sh
+++ setup/vscode.sh
@@ -6,16 +6,17 @@ echo
 
 # Extensions
 code --install-extension alefragnani.Bookmarks
+code --install-extension byCedric.vscode-expo
 code --install-extension dbaeumer.vscode-eslint
-code --install-extension mgmcdermott.vscode-language-babel
+code --install-extension dracula-theme.theme-dracula
 code --install-extension esbenp.prettier-vscode
 code --install-extension file-icons.file-icons
 code --install-extension flowtype.flow-for-vscode
+code --install-extension mgmcdermott.vscode-language-babel
 code --install-extension Orta.vscode-jest
 code --install-extension skyapps.fish-vscode
 code --install-extension sourcegraph.sourcegraph
 code --install-extension streetsidesoftware.code-spell-checker
-code --install-extension dracula-theme.theme-dracula
 code --install-extension vsmobile.vscode-react-native
 
 # Settings
