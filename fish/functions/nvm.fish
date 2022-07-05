11940fb9022eaf6c42526d329b11170e5b74d303
Ian Hill <ianhill@buymeasoda.com>
2022-07-05 18:50:09 +1000

    Update fish nvm helper

--- fish/functions/nvm.fish
+++ fish/functions/nvm.fish
@@ -1,5 +1,5 @@
 function nvm --description 'Run bash nvm commands via fish shell'
-  bass source /usr/local/opt/nvm/nvm.sh --no-use ';' nvm $argv
+  bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
 end
 
 set -x NVM_DIR ~/.nvm
