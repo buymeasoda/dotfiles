1ca72dc12910ec391c89c439cee75591f92554b1
Ian Hill <ianhill@buymeasoda.com>
2022-09-27 11:29:45 +1000

    Remove several npm install apps

--- setup/npm.sh
+++ setup/npm.sh
@@ -7,9 +7,6 @@ echo
 # Install
 npm install -g browser-sync
 npm install -g eas-cli
-npm install -g expo-cli
 npm install -g http-server
-npm install -g jscodeshift
 npm install -g nodemon
-npm install -g prettier
 npm install -g sharp-cli
