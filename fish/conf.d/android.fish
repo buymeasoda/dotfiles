b34d24cdf35099366ed2ed262ca8827682bbdbc5
Ian Hill <ianhill@buymeasoda.com>
2023-01-22 16:31:30 +1000

    Remove unused android paths

--- fish/conf.d/android.fish
+++ fish/conf.d/android.fish
@@ -3,7 +3,4 @@ set -x ANDROID_HOME $HOME/Library/Android/sdk
 
 # Paths
 setpath $ANDROID_HOME/emulator
-setpath $ANDROID_HOME/tools
-setpath $ANDROID_HOME/cmdline-tools/latest/bin
-setpath $ANDROID_HOME/tools/bin
 setpath $ANDROID_HOME/platform-tools
