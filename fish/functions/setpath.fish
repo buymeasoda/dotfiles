ebe666e8e606f954f688d3f3095f280a71a678b2
Ian Hill <ianhill@buymeasoda.com>
2023-01-22 16:34:12 +1000

    Remove custom setpath function

--- fish/functions/setpath.fish
+++
@@ -1,7 +0,0 @@
-function setpath --description 'Add new path to environment without duplication'
-  for new_path in $argv
-    if not contains $new_path $PATH
-      set -x PATH $PATH $new_path
-    end
-  end
-end
