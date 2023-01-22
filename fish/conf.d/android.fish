d5898cc3e5550c98384093782f72668d9d10d136
Ian Hill <ianhill@buymeasoda.com>
2023-01-22 16:32:26 +1000

    Replace custom setpath function with fish built in

--- fish/conf.d/android.fish
+++ fish/conf.d/android.fish
@@ -2,5 +2,5 @@
 set -x ANDROID_HOME $HOME/Library/Android/sdk
 
 # Paths
-setpath $ANDROID_HOME/emulator
-setpath $ANDROID_HOME/platform-tools
+fish_add_path --path --append $ANDROID_HOME/emulator
+fish_add_path --path --append $ANDROID_HOME/platform-tools
