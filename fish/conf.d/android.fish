f12dfadd75741239890d2e4150f0d7b5d93885bf
Ian Hill <ianhill@buymeasoda.com>
2024-01-21 11:02:18 +1000

    Add JAVA_HOME config

--- fish/conf.d/android.fish
+++ fish/conf.d/android.fish
@@ -1,6 +1,9 @@
 # Android Tools (Install via Android Studio)
 set -x ANDROID_HOME $HOME/Library/Android/sdk
 
+# Java
+set -x JAVA_HOME /Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
+
 # Paths
 fish_add_path --path --append $ANDROID_HOME/emulator
 fish_add_path --path --append $ANDROID_HOME/platform-tools
