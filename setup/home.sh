7f4aac273ef0a618286a6d7629ba3c53c0b51613
Ian Hill <ianhill@buymeasoda.com>
2024-02-06 21:07:34 +1000

    Remove .hgrc file

--- setup/home.sh
+++ setup/home.sh
@@ -12,9 +12,6 @@ ln -sf ~/.dotfiles/home/.prettierrc ~/.prettierrc
 ln -sf ~/.dotfiles/home/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/home/.gitignore ~/.gitignore
 
-# Mercurial
-ln -sf ~/.dotfiles/home/.hgrc ~/.hgrc
-
 # Vim
 ln -sf ~/.dotfiles/home/.vim ~/.vim
 ln -sf ~/.dotfiles/home/.vimrc ~/.vimrc
