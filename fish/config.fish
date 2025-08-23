b6205595b6c03cb30ecf38d9d9382dfe5195f74b
Ian Hill <ianhill@buymeasoda.com>
2025-08-23 23:00:10 +1000

    Add starship config variant for Terminal.app

--- fish/config.fish
+++ fish/config.fish
@@ -3,9 +3,6 @@ set -g fish_greeting
 set -g fish_key_bindings fish_default_key_bindings
 set -g fish_prompt_pwd_dir_length 0
 
-# Theme
-fish_config theme choose "Catppuccin Mocha"
-
 # Editor
 set -x VISUAL /usr/bin/nano
 set -x EDITOR /usr/bin/nano
@@ -16,6 +13,12 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # OrbStack
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+# Theme
+fish_config theme choose "Catppuccin Mocha"
+if test $TERM_PROGRAM = "Apple_Terminal"
+  set -gx STARSHIP_CONFIG ~/.dotfiles/terminal/starship-terminal.toml
+end
 
 # Starship
 starship init fish | source
